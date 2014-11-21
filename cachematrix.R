005c13ea7141f0a1178c20b4f67841f5b9db02e8
Alex Mishin <mishin1990@gmail.com>
2014-11-21 11:25:16 +0200

    Assignment Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The Function pairs allows the Creation of a presistent matrix,
+## And Caching it's inverse for further use.
 
-## Write a short comment describing this function
 
+## Creates a presistent matrix, Which and retains the inverse matrix.
+## Used for Caching (does not calculate the inverse, just retains it)
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Takes as input a list created by makeCacheMatrix, 
+## and returns the cached inverse if exsits.
+## Otherwise calculates the inverse, addes it to the cachedMatrix, and returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+        
 }
